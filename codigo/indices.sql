261531e4c7796e8238d20fd67b44773e8b6aab8c
Yosua Blanco <66221986+yosuabdiaz@users.noreply.github.com>
2021-01-11 23:57:22 -0600

    indices

---
+++ codigo/indices.sql
@@ -0,0 +1,5 @@
+CREATE INDEX nombre ON cliente("nombre");
+CREATE INDEX razon_comercial ON distribuidor("razon_comercial");
+CREATE INDEX nombre_empleado ON empleado("nombre");
+CREATE INDEX factura_electronica ON factura_distribuidor("factura_electronica");
+CREATE INDEX nombre_producto ON producto("descripcion");
